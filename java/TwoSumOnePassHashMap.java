8d9dacd3f29376898e2427fb5b6fed83e2fe62e8
Deborshi Goswami <33585773+deborshigoswami95@users.noreply.github.com>
2021-05-18 20:18:06 -0700

    Create TwoSumOnePassHashMap.java

---
+++ java/TwoSumOnePassHashMap.java
@@ -0,0 +1,18 @@
+class TwoSumOnePassHashMap {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> m = new HashMap<>();
+        int other;
+        
+        for (int i=0; i<nums.length; i++){
+            other = target - nums[i];
+            if (m.containsKey(other)){
+                return new int []{m.get(other), i};
+            }
+            m.put(nums[i], i);
+        }
+   
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
