cb52867739dde1d3427d09adf90bba2fa5b068d1
Deborshi Goswami <33585773+deborshigoswami95@users.noreply.github.com>
2021-05-24 00:07:52 -0700

    Create generateParanthesesTree.java

---
+++ java/generateParanthesesTree.java
@@ -0,0 +1,27 @@
+class generateParanthesesTree {
+    private List<String> sol = new ArrayList<>();
+    
+    public List<String> generateParenthesis(int n) {
+        int i = n;
+        int j = 0;
+        String s = "";
+        this.recursiveParanthesis(i,j,s);
+        return sol;
+    }
+  
+    private void recursiveParanthesis(int i, int j, String s) {
+        if (i==0 & j==0) {
+            this.sol.add(s);
+        }
+        else if (i>0 & j==0) {
+            recursiveParanthesis(i-1,1,s+"(");
+        }
+        else if (i==0 & j>0) {
+            recursiveParanthesis(0,j-1,s+")");
+        }
+        else if (i>0 & j>0) {
+            recursiveParanthesis(i-1, j+1, s+"(");
+            recursiveParanthesis(i, j-1, s+")");
+        }
+    }
+}
